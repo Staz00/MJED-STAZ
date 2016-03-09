6a66a28ebc58fc6b31bc7ba76bdf47b97e6b1c2b
bluelight20 <blanchitachiin@gmail.com>
2016-03-09 17:23:48 +1300

    Added a new class from Asus Tablet

---
+++ GitHubText/GitHubText/NewClassFromTablet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubText
+{
+    public class NewClassFromTablet
+    {
+        string name;
+
+        public NewClassFromTablet(string _name)
+        {
+            name = _name;
+        }
+    }
+}
