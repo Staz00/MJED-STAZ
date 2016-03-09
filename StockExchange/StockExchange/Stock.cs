d5a8c4a79dfd8213538997d61346741fbb332880
bluelight20 <blanchitachiin@gmail.com>
2016-03-09 23:15:38 +1300

    Start of something

---
+++ StockExchange/StockExchange/Stock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchange
+{
+    public class Stock
+    {
+        private int baseStock;
+        private int stockMultiplier;
+
+        private string name;
+
+        public Stock()
+        {
+
+        }
+
+        public Stock(string _name, int _stock)
+        {
+            baseStock = _stock;
+            name = _name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetMultiplier(int multiplier)
+        {
+            stockMultiplier = multiplier;
+        }
+
+        public int GetStock()
+        {
+            return baseStock;
+        }
+       
+        public int GetStockPrice()
+        {
+            if (stockMultiplier == 0)
+                return 1;
+
+            return baseStock * stockMultiplier;
+        }
+
+    }
+}
